0b796e2e0902f17e4deaee11a03c5f93db2295df
jihan@200107 <jihanelhssisen200107@gmail.com>
2023-04-21 00:26:38 +0200

    controllers-donation-stock

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class StockController extends Controller
 {
-    //
+        public function index()
+    {
+        // Afficher la liste des stocks disponibles
+    }
+
+    public function create()
+    {
+        // Afficher le formulaire de création de stock
+    }
+
+    public function store(Request $request)
+    {
+        // Ajouter un nouveau stock
+    }
+
+    public function edit($id)
+    {
+        // Afficher le formulaire d'édition du stock sélectionné
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Mettre à jour les informations du stock sélectionné
+    }
+
+    public function destroy($id)
+    {
+        // Supprimer le stock sélectionné
+    }
+
 }
---
+++ database/migrations/2023_04_20_220838_create_donations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('amount', 10, 2);
+            $table->string('donation_type');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
+
