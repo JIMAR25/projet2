7332b7ccf83c07aa7aac49d6d760fde4000227b2
jihan@200107 <jihanelhssisen200107@gmail.com>
2023-04-21 01:27:48 +0200

    stock-profile-donation

---
+++ app/Http/Controllers/DonationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DonationController extends Controller
+{
+        public function index()
+    {
+        // Afficher la page de donation en ligne
+    }
+
+    public function store(Request $request)
+    {
+        // Traiter la donation en ligne
+    }
+}
