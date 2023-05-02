8b90612e772efa20a4c5bb24acc31868da3f520c
jihan@200107 <jihanelhssisen200107@gmail.com>
2023-05-02 14:07:26 +0200

    footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,161 @@
+<footer>
+        <section class="footer">
+            <div class="footer-top">
+              <div class="hfoot">
+                <h4>Comment vos dons sont utilisés</h4>
+                <p>
+                  Vos dons financent des millions de consultations,d'interventions <br> chirurgicales, de traitements et
+                   de vaccinations chaque année.
+                </p>
+              </div>
+                <div class="pource">
+                  <span>50%</span>
+                  <p>Votre nombre ici</p>
+                </div>
+                <div class="pource">
+                  <span>75%</span>
+                  <p>Votre nombre ici</p>
+                </div>
+                <div class="pource">
+                  <span>100%</span>
+                  <p>Votre nombre ici</p>
+                </div>
+            </div>
+            <hr>
+            <div class="box-container">
+                <div class="box2">
+                    <span class="flaticon-solidarity"></span>
+                    <h2 class="navbar-brand">ShareHope</h2>
+                </div>
+                <div class="box2">
+                    <h3>{{__('Liens Rapides')}}</h3>
+                    <a href="#" class="a1">{{__('Accueil')}}</a>
+                    <a href="#">{{__('Doantions')}}</a>
+                    <a href="#">{{__('Benevolats')}}</a>
+                    <a href="#">{{__('Evenements')}}</a>
+                    <a href="#">{{__('À propos de nous')}}</a>
+                    <a href="#">{{__('Services')}}</a>
+                </div>
+                <div class="box2">
+                    <h3>{{__('Liens Supplémentaires')}}</h3>
+                    <a href="#"> <i class="fas fa-angle-right"></i> {{__('Poser des questions')}}</a>
+                    <a href="#"> <i class="fas fa-angle-right"></i>{{__('fjbdsjhvsbdskvjbs')}}</a>
+                    <a href="#"> <i class="fas fa-angle-right"></i>{{__('fergnekrgnvfkvj')}}</a>
+                    <a href="#"> <i class="fas fa-angle-right"></i> {{__('4urfbui43fwkeb4f')}}</a>
+                </div>
+                <div class="box2">
+                    <h3>{{__('Infos de contact')}}</h3>
+                    <a href="#"> <i class="fas fa-phone"></i>+212 546372891</a>
+                    <a href="#"> <i class="fas fa-phone"></i> +212 546738292 </a>
+                    <a href="#"> <i class="fas fa-envelope"></i>sharehope@gmail.com</a>
+                    <a href="#"> <i class="fas fa-map"></i>av Tetouan N 20 Tetouan Maroc</a>
+                </div>
+                <div class="box2">
+                    <h3>{{__('Suivez-nous')}}</h3>
+                    <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+                    <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+                    <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+                    <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+                </div>
+            </div>
+            <div class="credit">© 2023<span> ShareHope </span> | {{__('Tous les droits sont réservés!')}} </div>
+        </section>
+        </footer>
+        <style>
+            footer {
+  background-color:#696969;
+  padding: 50px;
+  color:white;
+}
+
+.box-container {
+  display: flex;
+  
+  gap:120px;
+}
+
+.box2 {
+  width: 25%;
+  padding: 10px;
+  margin-bottom: 30px;
+}
+
+.box2 h3 {
+  font-size: 1.2em;
+  margin-bottom: 15px;
+  color:#B22222;
+}
+
+.box2 a {
+  display: block;
+  margin-bottom: 5px;
+  text-decoration: none;
+  color:#DCDCDC;
+}
+
+.box2 a:hover {
+  color:white;
+}
+
+.credit {
+  margin-top: 50px;
+  text-align: center;
+  font-size: 0.8em;
+  color: #999;
+}
+
+.credit span {
+  font-weight: bold;
+  color: #333;
+}
+
+.flaticon-solidarity {
+  background-image: url('pics/solidarite.png');
+  background-size: contain;
+  width: 4em;
+  height: 4em;
+  margin-top:50px;
+  display: inline-block;
+  vertical-align: middle;
+
+}
+.box2 h2{
+  color:white;
+  margin-top:-16.5px;
+margin-left:50px;
+  font-family:"Comic Sans MS";
+}
+
+.footer-top {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 30px;
+}
+
+.footer-top p {
+  margin: 0;
+}
+
+.footer-top span {
+  font-size: 24px;
+  font-weight: 700;
+  margin-left: 10px;
+}
+
+.footer-top hr {
+  height: 1px;
+  width: 100%;
+  margin: 0;
+  border: none;
+  background-color: #d1d1d1;
+}
+.hfoot h4{
+  font-family:Lucida Console;
+  font-family:2rem;
+}
+.pource span{
+  font-size:4.5rem;
+  font-weight:bold;
+}
+        </style>
